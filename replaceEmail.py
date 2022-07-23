20ff538d009ac854d01ffd0f299322297b735516
iamsayali <sayaliambure15@gmail.com>
2022-07-23 12:13:07 +0530

    Create replaceEmail.py
    Replacing a email

---
+++ replaceEmail.py
@@ -0,0 +1,28 @@
+# #program to replace an email with old domain name with a email with new domain name
+
+# '''old_domain = "yahoo.com"
+# new_domain = "gmail.com"     '''
+
+
+# def replace_email(email, old_domain, new_domain):
+#     if "@" + old_domain in email:
+#         index = email.index("@" + old_domain)
+#         new_email = email.index[ :index] + "@" +new_domain
+#         return new_email
+#     return email
+
+# #print(replace_email("sayaliambure15@yahoo.com"))   
+
+
+
+# print("no of times e occured in this text".count("e"))
+# print("forest".endswith("rest"))
+# print("123242".isnumeric())
+# print("namaste".isnumeric())
+
+# #join method
+name = " ".join(["list", "elements", "joined"])
+print(name)
+
+naam = "This sting is going to be splitted"
+print(naam.split())
